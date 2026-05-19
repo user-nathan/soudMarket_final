bda3409cb380212d06be35a340d72c6df7e460ea
user_nathan <jonathan.samueza.quinatoa@gmail.com>
2026-05-19 02:02:40 +0200

    cambio 2 de url index

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 // public/index.php
 
 // 1. Cargamos los archivos principales
-require_once '/app/core/database.php';
+require_once __DIR__ . '/../app/core/database.php';
 require_once '../app/core/enrutador.php';
 
 // 2. Iniciamos el enrutador
